2f7f86184f014d85a34c6f33b69e6cccd686fb86
s0ulless <s0ulless@users.noreply.github.com>
2026-04-26 11:02:37 +0800

    Added server.py.

---
+++ server.py
@@ -0,0 +1,90 @@
+##from flask import Flask, request, jsonify, render_template
+##from EmotionDetection.emotion_detection import emotion_detector
+##
+####app = Flask("Final Proj Emo Detector")
+####
+####@app.route('/')
+####def index():
+####    # Serve index.html from /templates
+####    return render_template('index.html')
+####
+####@app.route('/emotionDetector', methods=['GET', 'POST'])
+####def detect_emotion():
+####    if request.method == 'GET':
+####        text_to_analyze = request.args.get('textToAnalyze', '')
+####    else:  # POST
+####        data = request.get_json()
+####        text_to_analyze = data.get('text', '')
+####
+####    result = emotion_detector(text_to_analyze)
+####    return jsonify(result)
+####
+####if __name__ == '__main__':
+####    app.run(host='0.0.0.0', port=5000, debug=True)
+##
+### 7b
+####from flask import Flask, request, jsonify, render_template
+####from EmotionDetection.emotion_detection import emotion_detector
+####
+####app = Flask("Final Proj Emo Detector")
+####
+####@app.route('/')
+####def index():
+####    return render_template('index.html')
+####
+####@app.route('/emotionDetector', methods=['GET', 'POST'])
+####def detect_emotion():
+####    # Handle GET (query parameter) and POST (JSON body)
+####    if request.method == 'GET':
+####        text_to_analyze = request.args.get('textToAnalyze', '')
+####    else:  # POST
+####        data = request.get_json()
+####        text_to_analyze = data.get('text', '')
+####
+####    result = emotion_detector(text_to_analyze)
+####
+####    # Error handling: if dominant_emotion is None, return error message
+####    if result['dominant_emotion'] is None:
+####        return jsonify({"message": "Invalid text! Please try again!"})
+####
+####    return jsonify(result)
+####
+####if __name__ == '__main__':
+####    app.run(host='0.0.0.0', port=5000, debug=True)
+
+"""
+# Server Code (Pylint-compliant)
+from flask import Flask, request, jsonify, render_template
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Final Proj Emo Detector")
+
+@app.route('/')
+def index():
+    """
+    Renders index.html
+    """
+    return render_template('index.html')
+
+@app.route('/emotionDetector', methods=['GET', 'POST'])
+def detect_emotion():
+    """
+    Handle GET (query parameter) and POST (JSON body)
+    """
+    if request.method == 'GET':
+        text_to_analyze = request.args.get('textToAnalyze', '')
+    else:  # POST
+        data = request.get_json()
+        text_to_analyze = data.get('text', '')
+
+    result = emotion_detector(text_to_analyze)
+
+    # Error handling: if dominant_emotion is None, return error message
+    if result['dominant_emotion'] is None:
+        return jsonify({"message": "Invalid text! Please try again!"})
+
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+"""
